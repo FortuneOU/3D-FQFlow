3b5d618ca658d534e8c58e68660a9257d4dd2c0e
FortuneOU <114125276+FortuneOU@users.noreply.github.com>
2026-03-22 23:05:10 +0800

    Update Scene.m

--- Code/+simcore/Scene.m
+++ Code/+simcore/Scene.m
@@ -1,7 +1,7 @@
 classdef Scene
     properties
         MotionMode       % Type of motion model selected
-        ThreeDMode       % True if 3D reconstruction is used
+        %ThreeDMode       % True if 3D reconstruction is used
         probeCase        % Probe type
         phantomCase      % Phantom type
         velocityFieldMode% Type of background motion field
@@ -16,11 +16,15 @@ classdef Scene
                     param = getparam('L11-5v');
                     param.c = 1540;
                     [phantomPoints(:,1),phantomPoints(:,2),phantomPoints(:,3)] = ...
-                        genscat([4 5 0.2]*1e-2,param); % scatterer density
+                        genscat([4 6 1.5]*1e-2,param); % scatterer density
                 case 'Vermon'
                     param.c = 1540; param.fc = 7.81e6; param.bandwidth = 70;
                     [phantomPoints(:,1),phantomPoints(:,2),phantomPoints(:,3)] = ...
                         genscat([2.1 2.6 2.1]*1e-2,param);
+                case 'Vermon-3'
+                    param.c = 1540; param.fc = 3e6; param.bandwidth = 70;
+                    [phantomPoints(:,1),phantomPoints(:,2),phantomPoints(:,3)] = ...
+                        genscat([4.1 8.1 4.1]*1e-2,param);
                 otherwise
                     error('Unknown probeCase');
             end
@@ -42,6 +46,42 @@ classdef Scene
                     obj.T = utils.spaceTransform([1 2 3], ...
                         [0,0,0]*1e-3, ...
                         [40,45,-50]*1e-3, "GlnTran");
+                case 'T1Trace'
+                    flowFileFolder = '..\Data\T1Trace';
+                    obj.T = utils.spaceTransform([1 2 3], ...
+                        [0,0,75]*1e-3, ...
+                        [0,-pi/6,0], ...
+                        [0,0,25]*1e-3, "T1Trace");
+                case 'T2Trace'
+                    flowFileFolder = '..\Data\T2Trace';
+                    obj.T = utils.spaceTransform([1 3 2], ...
+                        [0,0,0]*1e-3, ...
+                        [0,0,0], ...
+                        [7,60,55]*1e-3, "T2Trace");
+                case 'T2Trace_1'
+                    flowFileFolder = '..\Data\T2Trace';
+                    obj.T = utils.spaceTransform([1 3 2], ...
+                        [0,0,0]*1e-3, ...
+                        [0,0,0], ...
+                        [7,50,42]*1e-3, "T2Trace_1");
+                case 'T3Trace'
+                    flowFileFolder = '..\Data\T3Trace';
+                    obj.T = utils.spaceTransform([1 3 2], ...
+                        [0,0,0]*1e-3, ...
+                        [0,0,0], ...
+                        [7,50,42]*1e-3, "T2Trace_1");
+                 case 'F2Trace'
+                   flowFileFolder = '..\Data\F2Trace';
+                    obj.T = utils.spaceTransform([1 2 3], ...
+                        [0,0,0]*1e-3, ...
+                        [0,0,0], ...
+                        [0,25,20]*1e-3, "F2Trace");
+                 case 'F20Trace'
+                   flowFileFolder = '..\Data\F20Trace';
+                    obj.T = utils.spaceTransform([1 2 3], ...
+                        [0,0,0]*1e-3, ...
+                        [0,0,0], ...
+                        [0,25,20]*1e-3, 0.1,"F20Trace");
                 otherwise
                     error('Unknown phantomCase');
             end
@@ -50,14 +90,25 @@ classdef Scene
             vtufilepath = fullfile(flowFileFolder,'vtu.mat');
             GeometryPropertiesPath = fullfile(flowFileFolder,'GeometryProperties.mat');
             load(GeometryPropertiesPath,'vtuProperties');
-            [~, Grid] = alg.load_vessel_data(vtufilepath,vtuProperties);
+            [~, Grid] = alg.load_vessel_data_FQ(vtufilepath,vtuProperties);
 
             % Transform scatterer positions to vessel grid coordinates
             phantomPointsInGrid = obj.T.invTransform(phantomPoints);
-            inVesselFlag = alg.get_vtu_indices(phantomPointsInGrid,Grid);
+            switch globalParam.phantomCase
+                case  'T1Trace'
 
+                    [~,outsideGrid] = alg.get_vtu_indices4vtu(phantomPointsInGrid,Grid);
+                    obj.tissueScatters = phantomPoints(outsideGrid,:);
+                case  'T2Trace'
+                    [~,outsideGrid] = alg.get_vtu_indices4vtu(phantomPointsInGrid,Grid);
+                    obj.tissueScatters = phantomPoints(outsideGrid,:);
+
+                otherwise
+                    inVesselFlag = alg.get_vtu_indices(phantomPointsInGrid,Grid);
+                    obj.tissueScatters = phantomPoints((inVesselFlag == 0),:);
+            end
             % Keep only tissue scatterers outside vessel lumen
-            obj.tissueScatters = phantomPoints((inVesselFlag == 0),:);
+
 
             % Store setup params
             obj.MotionMode       = globalParam.MotionMode;
